804bcec73c63675032fe29c583fe7ce67c3f21ca
dakeloon <94038117+dakeloon@users.noreply.github.com>
2022-04-25 10:55:19 +0700

    Update README.cs

--- README.cs
+++ README.cs
@@ -64,3 +64,76 @@ namespace Serialization
             File.Move($"C:/Users/student/source/repos/Serialization/Serialization/bin/Debug/netcoreapp3.1/{FileName}Copy.txt", $"C:/Users/student/source/repos/Serialization/Serialization/bin/Debug/netcoreapp3.1/{FileName}.txt");
             Console.WriteLine("Rollback was successful");
         }
+ static void Main(string[] args)
+        {
+            // object for serialize
+            TextFile WordFile = new TextFile("Simi");
+            Console.WriteLine("Object created");
+
+            // create a BinaryFormatter object
+            BinaryFormatter Formatter = new BinaryFormatter();
+
+            // serialization to files.dat
+            using (FileStream StreamFile = new FileStream("files.dat", FileMode.OpenOrCreate))
+            {
+                Formatter.Serialize(StreamFile, WordFile);
+
+                Console.WriteLine("Object serialized");
+            }
+
+            // deserialization from files.dat
+            using (FileStream StreamFile = new FileStream("files.dat", FileMode.OpenOrCreate))
+            {
+                TextFile NewWordFile = (TextFile)Formatter.Deserialize(StreamFile);
+
+                Console.WriteLine("Object deserialized");
+                Console.WriteLine($"Author: {NewWordFile.Author}");
+            }
+
+            // enter keyword
+            Console.Write("Enter keyword: ");
+            string Path = Console.ReadLine();
+
+            // search file and enter info
+            FileInfo FileInformation = new FileInfo($"C:/Users/student/source/repos/Serialization/Serialization/bin/Debug/netcoreapp3.1/{Path}.txt");
+            if (FileInformation.Exists)
+            {
+                Console.WriteLine($"File name: {FileInformation.Name}");
+                Console.WriteLine($"Time of creation: {FileInformation.CreationTime}");
+                Console.WriteLine($"Size: {FileInformation.Length}");
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+
+            // menu
+            bool OpenMenu = true;
+            do
+            {
+                Console.WriteLine("***********       Menu       ***********");
+                Console.WriteLine("Create file to write text -- 1\nOpen created file for reading -- 2\nRollback changes -- 3\nExit -- 4");
+                Console.WriteLine("****************************************");
+
+                switch (Int16.Parse(Console.ReadLine()))
+                {
+                    case 1:
+                        CreateFile();
+                        break;
+                    case 2:
+                        OpenFile();
+                        break;
+                    case 3:
+                        RollbackChanges();
+                        break;
+                    case 4:
+                        OpenMenu = false;
+                        break;
+                    default:
+                        Console.WriteLine("Error");
+                        break;
+                }
+            } while (OpenMenu);
+        }
+    }
+}
